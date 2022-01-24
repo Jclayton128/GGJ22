44ca5744d52eb28abc896f0a7a7ed9aba5e63855
Tony Li <tonyli99@users.noreply.github.com>
2022-01-23 20:12:11 -0500

    Added ColonistCount, TechLevel, and Morale to Google Sheets & CardLibrary

--- Assets/Scripts/Cards/CardLibrary.cs
+++ Assets/Scripts/Cards/CardLibrary.cs
@@ -12,6 +12,9 @@ public class CardLibrary
     private Dictionary<int, List<Card>> cards = null;
     private int next = 0;
 
+    private const int NumColumns = 22;
+    private const int NumParameters = 7;
+
     private static Card DefaultCard = new Card(
         "Default",
         "",
@@ -19,11 +22,11 @@ public class CardLibrary
         new Option(
             "Option A",
             "Option A Result",
-            new int[4]),
+            new int[NumParameters]),
         new Option(
             "Option B",
             "Option B Result",
-            new int[4])
+            new int[NumParameters])
         );
 
     public CardLibrary(TextAsset csv)
@@ -44,7 +47,7 @@ public class CardLibrary
     {
         foreach (List<string> columns in rows)
         {
-            if (columns.Count < 16) continue;
+            if (columns.Count < NumColumns) continue;
             int phase;
             Card card = ColumnsToCard(columns, out phase);
             if (!cards.ContainsKey(phase)) cards[phase] = new List<Card>();
@@ -54,18 +57,21 @@ public class CardLibrary
 
     private Card ColumnsToCard(List<string> columns, out int phase)
     {
-        // 0     1  2      3        4 5        6           7           8             9            10 11       12          13          14            15
-        // Phase,ID,Prereq,Scenario,A,A.Result,A.Community,A.Tradition,A.Exploration,A.Objectivity,B,B.Result,B.Community,B.Tradition,B.Exploration,B.Objectivity
+        // 0     1  2      3        4 5        6           7           8             9             10           11          12      
+        // Phase,ID,Prereq,Scenario,A,A.Result,A.Community,A.Tradition,A.Exploration,A.Objectivity,A.Population,A.TechLevel,A.Morale,
+        //
+        //13 14       15          16          17            18            19           20          21
+        // B,B.Result,B.Community,B.Tradition,B.Exploration,B.Objectivity,B.Population,B.TechLevel,B.Morale
         phase = int.Parse(columns[0]);
         string id = columns[1];
         string prereq = columns[2];
         string mainText = columns[3];
         string optAText = columns[4];
         string optAResult = columns[5];
-        Option optionA = new Option(optAText, optAResult, new int[] { int.Parse(columns[6]), int.Parse(columns[7]), int.Parse(columns[8]), int.Parse(columns[9]) });
-        string optBText = columns[10];
-        string optBResult = columns[11];
-        Option optionB = new Option(optBText, optBResult, new int[] { int.Parse(columns[12]), int.Parse(columns[13]), int.Parse(columns[14]), int.Parse(columns[15]) });
+        Option optionA = new Option(optAText, optAResult, new int[] { int.Parse(columns[6]), int.Parse(columns[7]), int.Parse(columns[8]), int.Parse(columns[9]), int.Parse(columns[10]), int.Parse(columns[11]), int.Parse(columns[12]) });
+        string optBText = columns[13];
+        string optBResult = columns[14];
+        Option optionB = new Option(optBText, optBResult, new int[] { int.Parse(columns[15]), int.Parse(columns[16]), int.Parse(columns[17]), int.Parse(columns[18]), int.Parse(columns[19]), int.Parse(columns[20]), int.Parse(columns[21]) });
         return new Card(id, prereq, mainText, optionA, optionB);
     }
 
